81f05ea792a61c7c534488e82539cf51d033d25c
Mario fernando ortiz oviedo <mfortiz48@hotmail.com>
2021-11-10 21:00:58 -0500

    gfhghfgh

---
+++ TecniGymService/TecnygimService/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TecnygimService.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
